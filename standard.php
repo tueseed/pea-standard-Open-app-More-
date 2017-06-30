05b974cd39ac6202af7552ac72a60a1dcfb8dcd4
tueseed <tue_seed@hotmail.com>
2017-06-30 13:34:54 +0700

    Update standard.php

--- standard.php
+++ standard.php
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text.":".$Myd    //."  [".$KVA." KVA]"
+				'text' => $Myd    //."  [".$KVA." KVA]"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
