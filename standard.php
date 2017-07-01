2fceb3d3fdc4ec8974b9b434b657d5a0a512114f
tueseed <tue_seed@hotmail.com>
2017-07-01 16:32:15 +0700

    Update standard.php

--- standard.php
+++ standard.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			//$findName = strtoupper($findName);
             foreach($csv as $values)
             {
-             if($values[1]==$findName)   // index 0 contains the name
+             if($values[1]==$findName or $values[2]==$findName)   // index 0 contains the name
                  $Myd = iconv("tis-620","utf-8",$values[0]);
                  	                     // index 1 contains the googlemap link 
 			 
