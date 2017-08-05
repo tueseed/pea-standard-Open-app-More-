4cb66cbac6a5c7180a47f9b44f64e33ab1a0af81
tueseed <tue_seed@hotmail.com>
2017-08-05 19:47:06 +0700

    Update cms.php

--- cms.php
+++ cms.php
@@ -16,7 +16,8 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			
             $csv = array_map('str_getcsv', file('CMS.csv'));
-            $findName = iconv("utf-8","tis-620",$text);
+           // $findName = iconv("utf-8","tis-620",$text);
+            $findName = $text;
 			//$findName = strtoupper($findName);
             foreach($csv as $values)
             {
