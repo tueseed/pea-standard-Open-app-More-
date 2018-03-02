e46f600c701b8b6b124424476bf77e817d38ffc3
tueseed <tue_seed@hotmail.com>
2018-03-02 22:01:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-echo "Hello LINE BOT";
+$url = parse_url(getenv("mysql://bdb2c368d1a6ad:09b374bf@us-cdbr-iron-east-05.cleardb.net/heroku_056efb00ca70c61?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+if($conn){
+echo "connect";
+
+}
+else{
+echo "error";
+
+}
+//echo "Hello LINE BOT";
+?>
