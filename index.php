c149edd7eea4184e30884ed8b53cb4403f6be306
tueseed <tue_seed@hotmail.com>
2018-03-20 11:44:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,5 +15,18 @@ else{
 echo "ไม่เชื่อมต่ออออออออ";
 
 }
+function DateThai($strDate){
+				$strYear = date("Y",strtotime($strDate))+543;
+				$strMonth= date("n",strtotime($strDate));
+				$strDay= date("j",strtotime($strDate));
+				//$strHour= date("H",strtotime($strDate));
+				//$strMinute= date("i",strtotime($strDate));
+				//$strSeconds= date("s",strtotime($strDate));
+				$strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+				$strMonthThai=$strMonthCut[$strMonth];
+				return "$strDay $strMonthThai $strYear";
+			}
+$strDate1 = date("yy-m-d");
+echo DateThai($strDate1);
 //echo "Hello LINE BOT";
 ?>
