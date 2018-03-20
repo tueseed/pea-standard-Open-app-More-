5d8c028a34b16dabaa39044e4bf1a116506dce1a
tueseed <tue_seed@hotmail.com>
2018-03-20 11:38:51 +0700

    Update notify.php

--- notify.php
+++ notify.php
@@ -28,8 +28,8 @@ function DateThai($strDate){
 				return "$strDay $strMonthThai $strYear";
 			}
 //$strDate = DateThai(Now());
-$strDate1 = date("yyyy-m-d");
-$msg_notify = "รายงานสรุปเรื่องร้องเรียนสายงานการไฟฟ้าภาค 4 ประจำวันที่ ".$strDate1."\n"."https://voc-bot.herokuapp.com/daily.php";
+$strDate1 = date("yy-m-d");
+$msg_notify = "รายงานสรุปเรื่องร้องเรียนสายงานการไฟฟ้าภาค 4 ประจำวันที่ ".DateThai($strDate1)."\n"."https://voc-bot.herokuapp.com/daily.php";
 $message = $msg_notify;
 $token = 'RDusZuUWgC6NyW8dZvHRoqLDmAmhvj11huWKJwOznlR';
 
