e0bd52d77f095f932a6a0355f1a383d9ac3c43a7
tueseed <tue_seed@hotmail.com>
2018-03-21 14:53:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,12 +19,15 @@ function DateThai($strDate){
 				$strYear = date("Y",strtotime($strDate))+543;
 				$strMonth= date("n",strtotime($strDate));
 				$strDay= date("j",strtotime($strDate));
+				$strthaiday = date("w",strtotime($strDate));
 				//$strHour= date("H",strtotime($strDate));
 				//$strMinute= date("i",strtotime($strDate));
 				//$strSeconds= date("s",strtotime($strDate));
 				$strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
 				$strMonthThai=$strMonthCut[$strMonth];
-				return "$strDay $strMonthThai $strYear";
+	                        $strthaiday_cut = Array("อาทิตย์","จันทร์","อังคาร","พุธ","พฤหัสบดี","ศุกร์","เสาร์");
+	                        $strthaiday_t = $strthaiday_cut[$strthaiday];
+				return "$strthaiday_t $strDay $strMonthThai $strYear";
 			}
 $strDate1 = date("Y-m-d");
 echo DateThai($strDate1);
