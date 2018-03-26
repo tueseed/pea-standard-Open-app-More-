e1cc919d346a8e2bf66a1a6f849dcf34c5c806db
tueseed <tue_seed@hotmail.com>
2018-03-26 18:53:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function DateThai($strDate){
 			}
 $day_test1 ="2018-04-06";
 $day_test = date(strtotime($day_test1));
-$sql = "SELECT * FROM tbl_holiday WHERE holiday_date = '2018-04-06'";
+$sql = "SELECT * FROM tbl_holiday WHERE holiday_date = '".$day_test1."'";
 $query = mysqli_query($conn,$sql);
 $result = mysqli_fetch_array($query);
 if(!$result){
